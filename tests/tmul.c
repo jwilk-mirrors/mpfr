jwilk-mirrors/mpfr

commit 9d9342894a46fb448da94bc31b6c40d70cfc9448

[mul.c] simplified the code and fixed a bug [tmul.c] added non-regression test for fixed bug
git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@11004 280ebfd0-de03-0410-8827-d642c229c3f4

src/mul.c
@@ -211,29 +211,32 @@ static int
 mpfr_mul_1 (mpfr_ptr a, mpfr_srcptr b, mpfr_srcptr c, mpfr_rnd_t rnd_mode,
               mpfr_prec_t p)
 {
-  mp_limb_t h;
+  mp_limb_t a0;
   mpfr_limb_ptr ap = MPFR_MANT(a);
-  mpfr_exp_t ax = MPFR_GET_EXP(b) + MPFR_GET_EXP(c);
+  mpfr_limb_ptr bp = MPFR_MANT(b);
+  mpfr_limb_ptr cp = MPFR_MANT(c);
+  mpfr_exp_t ax;
   mpfr_prec_t sh = GMP_NUMB_BITS - p;
   mp_limb_t rb, sb, mask = MPFR_LIMB_MASK(sh);
 
   /* When prec(b), prec(c) <= GMP_NUMB_BITS / 2, we could replace umul_ppmm
      by a limb multiplication as follows, but we assume umul_ppmm is as fast
      as a limb multiplication on modern processors:
-      h = (MPFR_MANT(b)[0] >> (GMP_NUMB_BITS / 2))
-        * (MPFR_MANT(c)[0] >> (GMP_NUMB_BITS / 2));
+      a0 = (bp[0] >> (GMP_NUMB_BITS / 2))
+        * (cp[0] >> (GMP_NUMB_BITS / 2));
       sb = 0;
   */
-  umul_ppmm (h, sb, MPFR_MANT(b)[0], MPFR_MANT(c)[0]);
-  if (h < MPFR_LIMB_HIGHBIT)
+  ax = MPFR_GET_EXP(b) + MPFR_GET_EXP(c);
+  umul_ppmm (a0, sb, bp[0], cp[0]);
+  if (a0 < MPFR_LIMB_HIGHBIT)
     {
       ax --;
-      h = (h << 1) | (sb >> (GMP_NUMB_BITS - 1));
+      a0 = (a0 << 1) | (sb >> (GMP_NUMB_BITS - 1));
       sb = sb << 1;
     }
-  rb = h & (MPFR_LIMB_ONE << (sh - 1));
-  sb |= (h & mask) ^ rb;
-  ap[0] = h & ~mask;
+  rb = a0 & (MPFR_LIMB_ONE << (sh - 1));
+  sb |= (a0 & mask) ^ rb;
+  ap[0] = a0 & ~mask;
 
   MPFR_SIGN(a) = MPFR_MULT_SIGN (MPFR_SIGN (b), MPFR_SIGN (c));
 
@@ -246,20 +249,16 @@ mpfr_mul_1 (mpfr_ptr a, mpfr_srcptr b, mpfr_srcptr c, mpfr_rnd_t rnd_mode,
      a >= 0.111...111[1]*2^(emin-1), there is no underflow. */
   if (MPFR_UNLIKELY(ax < __gmpfr_emin))
     {
-      /* for RNDN, mpfr_underflow always rounds away, thus for |a| <= 2^(emin-2)
-         we have to change to RNDZ */
-      if (rnd_mode == MPFR_RNDN)
-        {
-          if ((ax == __gmpfr_emin - 1) && (ap[0] == ~mask) && rb)
-            goto rounding; /* no underflow */
-          if (ax < __gmpfr_emin - 1 || (ap[0] == MPFR_LIMB_HIGHBIT && sb == 0))
-            rnd_mode = MPFR_RNDZ;
-        }
-      else if (!MPFR_IS_LIKE_RNDZ(rnd_mode, MPFR_IS_NEG (a)))
-        {
-          if ((ax == __gmpfr_emin - 1) && (ap[0] == ~mask) && (rb | sb))
-            goto rounding; /* no underflow */
-        }
+      if ((ax == __gmpfr_emin - 1) && (ap[0] == ~mask) &&
+          (rb || (sb && !MPFR_IS_LIKE_RNDZ(rnd_mode, MPFR_IS_NEG (a)))))
+        goto rounding; /* no underflow */
+      /* For RNDN, mpfr_underflow always rounds away, thus for |a| <= 2^(emin-2)
+         we have to change to RNDZ. This corresponds to:
+         (a) either ax < emin - 1
+         (b) or ax = emin - 1 and ap[0] = 1000....000 and rb = sb = 0 */
+      if (rnd_mode == MPFR_RNDN &&
+          (ax < __gmpfr_emin - 1 || (ap[0] == MPFR_LIMB_HIGHBIT && (rb | sb) == 0)))
+        rnd_mode = MPFR_RNDZ;
       return mpfr_underflow (a, rnd_mode, MPFR_SIGN(a));
     }
 

tests/tmul.c
@@ -728,6 +728,30 @@ test_underflow (mpfr_prec_t pmax)
   mpfr_clear (a0);
 }
 
+/* checks special case where no underflow should occur */
+static void
+bug20161209 (void)
+{
+  mpfr_exp_t emin;
+  mpfr_t x, y, z;
+
+  emin = mpfr_get_emin ();
+  set_emin (-1);
+  mpfr_init2 (x, 53);
+  mpfr_init2 (y, 53);
+  mpfr_init2 (z, 53);
+  mpfr_set_str_binary (x, "0.101000001E-1"); /* x = 321/2^10 */
+  mpfr_set_str_binary (y, "0.110011000010100101111000011011000111011000001E-1");
+  /* y = 28059810762433/2^46 */
+  /* x * y = (2^53+1)/2^56 = 0.001...000[1]000..., and should round to 0.25 */
+  mpfr_mul (z, x, y, MPFR_RNDN);
+  MPFR_ASSERTN(mpfr_cmp_ui_2exp (z, 1, -2) == 0);
+  mpfr_clear (x);
+  mpfr_clear (y);
+  mpfr_clear (z);
+  set_emin (emin);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -776,6 +800,7 @@ main (int argc, char *argv[])
 
   valgrind20110503 ();
   test_underflow (128);
+  bug20161209 ();
 
   tests_end_mpfr ();
   return 0;

commit 0067e8e666ad883e907acb260248d8b73ab34fdf

fixed bug in mpfr_mul_1() introduced in r11004 and added test case
git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@11016 280ebfd0-de03-0410-8827-d642c229c3f4

src/mul.c
@@ -250,7 +250,8 @@ mpfr_mul_1 (mpfr_ptr a, mpfr_srcptr b, mpfr_srcptr c, mpfr_rnd_t rnd_mode,
   if (MPFR_UNLIKELY(ax < __gmpfr_emin))
     {
       if ((ax == __gmpfr_emin - 1) && (ap[0] == ~mask) &&
-          (rb || (sb && !MPFR_IS_LIKE_RNDZ(rnd_mode, MPFR_IS_NEG (a)))))
+          ((rnd_mode == MPFR_RNDN && rb) ||
+           (!MPFR_IS_LIKE_RNDZ(rnd_mode, MPFR_IS_NEG (a)) && (rb | sb))))
         goto rounding; /* no underflow */
       /* For RNDN, mpfr_underflow always rounds away, thus for |a| <= 2^(emin-2)
          we have to change to RNDZ. This corresponds to:

tests/tmul.c
@@ -784,6 +784,62 @@ bug20161209 (void)
   set_emin (emin);
 }
 
+/* test for case in mpfr_mul_1() where:
+   ax = __gmpfr_emin - 1
+   ap[0] == ~mask
+   rnd_mode = MPFR_RNDZ.
+   Whatever the values of rb and sb, we should round to zero (underflow). */
+static void
+bug20161209a (void)
+{
+  mpfr_exp_t emin;
+  mpfr_t x, y, z;
+
+  emin = mpfr_get_emin ();
+  set_emin (-1);
+
+  mpfr_init2 (x, 53);
+  mpfr_init2 (y, 53);
+  mpfr_init2 (z, 53);
+
+  /* case rb = sb = 0 */
+  mpfr_set_str_binary (x, "0.11010010100110000110110011111E-1");
+  mpfr_set_str_binary (y, "0.1001101110011000110100001");
+  /* x = 441650591/2^30, y = 20394401/2^25 */
+  /* x * y = (2^53-1)/2^55 = 0.00111...111[0]000..., and should round to 0 */
+  mpfr_mul (z, x, y, MPFR_RNDZ);
+  MPFR_ASSERTN(mpfr_zero_p (z));
+
+  /* case rb = 1, sb = 0 */
+  mpfr_set_str_binary (x, "0.111111111000000000000000000111111111E-1");
+  mpfr_set_str_binary (y, "0.1000000001000000001");
+  /* x = 68585259519/2^37, y = 262657/2^19 */
+  /* x * y = (2^54-1)/2^56 = 0.00111...111[1]000..., and should round to 0 */
+  mpfr_mul (z, x, y, MPFR_RNDZ);
+  MPFR_ASSERTN(mpfr_zero_p (z));
+
+  /* case rb = 0, sb = 1 */
+  mpfr_set_str_binary (x, "0.110010011001011110001100100001000001E-1");
+  mpfr_set_str_binary (y, "0.10100010100010111101");
+  /* x = 541144371852^37, y = 665789/2^20 */
+  /* x * y = (2^55-3)/2^57 = 0.00111...111[0]100..., and should round to 0 */
+  mpfr_mul (z, x, y, MPFR_RNDZ);
+  MPFR_ASSERTN(mpfr_zero_p (z));
+
+  /* case rb = sb = 1 */
+  mpfr_set_str_binary (x, "0.10100110001001001010001111110010100111E-1");
+  mpfr_set_str_binary (y, "0.110001010011101001");
+  /* x = 178394823847/2^39, y = 201961/2^18 */
+  /* x * y = (2^55-1)/2^57 = 0.00111...111[1]100..., and should round to 0 */
+  mpfr_mul (z, x, y, MPFR_RNDZ);
+  MPFR_ASSERTN(mpfr_zero_p (z));
+
+  mpfr_clear (x);
+  mpfr_clear (y);
+  mpfr_clear (z);
+  set_emin (emin);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -833,6 +889,7 @@ main (int argc, char *argv[])
   valgrind20110503 ();
   test_underflow (128);
   bug20161209 ();
+  bug20161209a ();
 
   tests_end_mpfr ();
   return 0;