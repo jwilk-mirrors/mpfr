jwilk-mirrors/mpfr

commit 0d48fd664fc5b78086ad99ba0d488408a427a2d9

[src/get_str.c] Improved mpfr_get_str_ndigits code structure.
git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@14494 280ebfd0-de03-0410-8827-d642c229c3f4

src/get_str.c
@@ -2484,6 +2484,7 @@ mpfr_ceil_mul (mpfr_exp_t e, int beta, int i)
 size_t
 mpfr_get_str_ndigits (int b, mpfr_prec_t p)
 {
+  size_t ret;
   MPFR_SAVE_EXPO_DECL (expo);
 
   MPFR_ASSERTN (2 <= b && b <= 62);
@@ -2505,57 +2506,57 @@ mpfr_get_str_ndigits (int b, mpfr_prec_t p)
      1 + ceil(p*log(2)/log(b)) for p < 186564318007 (it returns one more
      for p=186564318007 and b=7 or 49) */
   MPFR_STAT_STATIC_ASSERT (MPFR_PREC_BITS >= 64 || MPFR_PREC_BITS <= 32);
-  if
+  if (
 #if MPFR_PREC_BITS >= 64
-  /* 64-bit numbers are supported by the C implementation, so that we can
-     use the large constant below. If MPFR_PREC_BITS <= 32, the condition
-     is always satisfied, so that we do not need any test. */
-    (MPFR_LIKELY (p < 186564318007))
+    /* 64-bit numbers are supported by the C implementation, so that we can
+       use the large constant below. */
+    MPFR_LIKELY (p < 186564318007)
 #else
-    (1)
+    /* Since MPFR_PREC_BITS <= 32, the above condition is always satisfied,
+       so that we do not need any test on p. */
+    1
 #endif
-  {
-    size_t ret = 1 + mpfr_ceil_mul (IS_POW2(b) ? p - 1 : p, b, 1);
-    MPFR_SAVE_EXPO_FREE (expo);
-    return ret;
-  }
+      )
+    ret = mpfr_ceil_mul (IS_POW2(b) ? p - 1 : p, b, 1);
+  else
+    {
+      /* p is large and b is not a power of two. The code below works
+         for any value of p and b, as long as b is not a power of two.
+         Indeed, in such a case, p*log(2)/log(b) cannot be exactly an
+         integer, and thus Ziv's loop will terminate. */
+      mpfr_prec_t w = 77; /* mpfr_ceil_mul used a 77-bit upper approximation
+                             to log(2)/log(b) */
+      do
+        {
+          mpfr_t d, u;
 
-  /* Now p is large and b is not a power of two. The code below works for any
-     value of p and b, as long as b is not a power of two. Indeed, in such a
-     case, p*log(2)/log(b) cannot be exactly an integer, and thus Ziv's loop
-     will terminate. */
-  {
-    mpfr_prec_t w = 77; /* mpfr_ceil_mul used a 77-bit upper approximation of
-                           log(2)/log(b) */
-    mpfr_t d, u;
-    size_t ret = 0;
-    while (ret == 0)
-      {
-        w = 2 * w;
-        mpfr_init2 (d, w); /* lower approximation */
-        mpfr_init2 (u, w); /* upper approximation */
-        mpfr_set_ui (d, b, MPFR_RNDU);
-        mpfr_set_ui (u, b, MPFR_RNDD);
-        mpfr_log2 (d, d, MPFR_RNDU);
-        mpfr_log2 (u, u, MPFR_RNDD);
-        /* The code below requires that the precision fit in an unsigned long,
-           which we currently guarantee (see _MPFR_PREC_FORMAT). */
-        MPFR_STAT_STATIC_ASSERT (MPFR_PREC_MAX <= ULONG_MAX);
-        /* u <= log(b)/log(2) <= d (***) */
-        mpfr_ui_div (d, p, d, MPFR_RNDD);
-        mpfr_ui_div (u, p, u, MPFR_RNDU);
-        /* d <= p*log(2)/log(b) <= u */
-        mpfr_ceil (d, d);
-        mpfr_ceil (u, u);
-        if (mpfr_equal_p (d, u))
-          ret = mpfr_get_ui (d, MPFR_RNDU);
-        mpfr_clear (d);
-        mpfr_clear (u);
-      }
+          w = 2 * w;
+          mpfr_init2 (d, w); /* lower approximation */
+          mpfr_init2 (u, w); /* upper approximation */
+          mpfr_set_ui (d, b, MPFR_RNDU);
+          mpfr_set_ui (u, b, MPFR_RNDD);
+          mpfr_log2 (d, d, MPFR_RNDU);
+          mpfr_log2 (u, u, MPFR_RNDD);
+          /* The code below requires that the precision p fit in
+             an unsigned long, which we currently guarantee (see
+             _MPFR_PREC_FORMAT). */
+          MPFR_STAT_STATIC_ASSERT (MPFR_PREC_MAX <= ULONG_MAX);
+          /* u <= log(b)/log(2) <= d (***) */
+          mpfr_ui_div (d, p, d, MPFR_RNDD);
+          mpfr_ui_div (u, p, u, MPFR_RNDU);
+          /* d <= p*log(2)/log(b) <= u */
+          mpfr_ceil (d, d);
+          mpfr_ceil (u, u);
+          if (mpfr_equal_p (d, u))
+            ret = mpfr_get_ui (d, MPFR_RNDU);
+          mpfr_clear (d);
+          mpfr_clear (u);
+        }
+      while (ret == 0);
+    }
 
-    MPFR_SAVE_EXPO_FREE (expo);
-    return 1 + ret;
-  }
+  MPFR_SAVE_EXPO_FREE (expo);
+  return 1 + ret;
 }
 
 /* prints the mantissa of x in the string s, and writes the corresponding

commit 12ef5ace193cc917d5d4876874e152174ba03def

[src/get_str.c] Fixed bug in mpfr_get_str_ndigits introduced in r14494.
Note: the issue (an uninitialized variable) was detected by GCC 6.5.0;
recent GCC versions miss the -Wmaybe-uninitialized warning.

git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@14496 280ebfd0-de03-0410-8827-d642c229c3f4

src/get_str.c
@@ -2526,7 +2526,9 @@ mpfr_get_str_ndigits (int b, mpfr_prec_t p)
          integer, and thus Ziv's loop will terminate. */
       mpfr_prec_t w = 77; /* mpfr_ceil_mul used a 77-bit upper approximation
                              to log(2)/log(b) */
-      do
+
+      ret = 0;
+      while (ret == 0)
         {
           mpfr_t d, u;
 
@@ -2555,7 +2557,6 @@ mpfr_get_str_ndigits (int b, mpfr_prec_t p)
           mpfr_clear (d);
           mpfr_clear (u);
         }
-      while (ret == 0);
     }
 
   MPFR_SAVE_EXPO_FREE (expo);